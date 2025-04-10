0a7a898a1eb294d4bf07884a6591179f0326fd4f
IvanWa <ifanugrh02@student.uns.ac.id>
2025-04-10 08:16:11 +0700

    🚀 chore: add cors

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
-import index from './routes/index.js';
+import indexRouter from "./routes/index.js";
+import cors from "cors";
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(clerkMiddleware());
 app.use(express.json());
-app.use('/', index)
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`App is running on port ${PORT}`);
 });
 
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,42 @@
-import { clerkClient, requireAuth } from '@clerk/express';
-import express from 'express';
-
+import { clerkClient, requireAuth } from "@clerk/express";
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-router.get('/public', (req, res) => {
-    res.send('Hello Public!');
-})
+router.get("/public", (req, res) => {
+  res.send("Hello Public!");
+});
 
-router.get('/private', requireAuth(), async (req, res) => {
-    const userId = req.auth?.userId;
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "Hello from test route!" });
+});
 
-    if (!userId) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-  
-    try {
-      const user = await clerkClient.users.getUser(userId);
-  
-      // Dapatkan username atau nama lain
-      const username = user.username; // opsional, bisa juga `user.firstName`, `user.emailAddresses[0].emailAddress`, dll
-  
-      res.json({
-        userId: user.id,
-        username,
-        email: user.emailAddresses[0]?.emailAddress,
-      });
-    } catch (err) {
-      console.error("Failed to fetch user", err);
-      res.status(500).json({ message: "Error fetching user info" });
-    }
+router.get("/private", requireAuth(), async (req, res) => {
+  const userId = req.auth?.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const user = await clerkClient.users.getUser(userId);
+
+    // Dapatkan username atau nama lain
+    const username = user.username; // opsional, bisa juga `user.firstName`, `user.emailAddresses[0].emailAddress`, dll
+
+    res.json({
+      userId: user.id,
+      username,
+      email: user.emailAddresses[0]?.emailAddress,
+    });
+  } catch (err) {
+    console.error("Failed to fetch user", err);
+    res.status(500).json({ message: "Error fetching user info" });
+  }
 });
 
 export default router;
